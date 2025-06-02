c9d0e4c78fef76ccb88849b306d3da3b7cb16524
michalkrawczyk <mkrwczyk.1@gmail.com>
2025-06-02 22:45:55 +0200

    deleted not needed already imports

--- RAG_supporters/prompts_templates/rag_verifiers.py
+++ RAG_supporters/prompts_templates/rag_verifiers.py
@@ -1,9 +1,3 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain, SequentialChain
-from langchain_core.output_parsers import StrOutputParser
-
-from excluded_drafts.rag_verifiers import FINAL_VERDICT_PROMPT
 
 SRC_COMPARE_PROMPT= """
 Task: You are acting as a domain expert. Given a question and two sources ([SOURCE 1] and [SOURCE 2]), your task is to choose the source that is better suited to answer the question from your expert perspective, inferring the relevant domain from the question and the content of the sources themselves.
