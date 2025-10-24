cf11f9d8a8054476fbac8380a347853fe47ee60c
michalkrawczyk <mkrwczyk.1@gmail.com>
2025-10-25 01:09:39 +0200

    Moved base_text_splitter to utils

--- RAG_supporters/utils/text_splitters.py
+++ RAG_supporters/utils/text_splitters.py
@@ -3,6 +3,17 @@
 import re
 from typing import List
 
+def base_text_splitter(text: str, max_words: int = 200):
+    """
+    Split text into chunks of max_words words
+    """
+    words = text.replace("\n", "").split(" ")
+    chunks = []
+    for i in range(0, len(words), max_words):
+        chunks.append(" ".join(words[i : i + max_words]))
+
+    return chunks
+
 
 def split_into_sentences(text: str) -> List[str]:
     """
